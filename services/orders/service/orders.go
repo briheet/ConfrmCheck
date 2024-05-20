7d3386b47a0b31c73bfacc39d192668c58e73316
Briheet Singh Yadav <briheetyadav@gmail.com>
2024-05-20 18:33:35 +0530

    create order and grpc

---
+++ services/orders/service/orders.go
@@ -0,0 +1,23 @@
+package service
+
+import (
+	"context"
+
+	"github.con/briheet/kitchen/services/common/genproto/orders"
+)
+
+var ordersDB = make([]*orders.Order, 0)
+
+type OrderService struct {
+	// orders
+	// dependency ingection
+}
+
+func NewOrderService() *OrderService {
+	return &OrderService{}
+}
+
+func (s *OrderService) CreateOrder(ctx context.Context, order *orders.Order) error {
+	ordersDB = append(ordersDB, order)
+	return nil
+}
--- services/orders/types/types.go
+++ services/orders/types/types.go
@@ -1,1 +1,11 @@
 package types
+
+import (
+	"context"
+
+	"github.con/briheet/kitchen/services/common/genproto/orders"
+)
+
+type OrderService interface {
+	CreateOrder(context.Context, *orders.Order) error
+}
